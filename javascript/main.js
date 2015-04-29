4d7b65c484a8965695f6ad7af6638a985ed7364f
AdamGMurphy <adam.murphy@mun.ca>
2015-04-28 23:17:35 -0230

    content hides/shows to reflect selected navigation item

--- javascript/main.js
+++ javascript/main.js
@@ -2,16 +2,19 @@ function navigate(destination) {
 	var selectedHTMLCollection = document.getElementsByClassName("nav-selected");
 	var selected = [].slice.call(selectedHTMLCollection);
 
-	var destElement = document.getElementById(destination + "-nav");
-
+	var destNavElement = document.getElementById(destination + "-nav");
+	var destContentElement = document.getElementById(destination + "-content");
+	
 	function unselectNavElement(element, index, array) {
 		var eleClass = element.className;
 		eleClass = eleClass.replace("nav-selected", "");
 		eleClass = eleClass.replace("  ", " ").trim();
 		element.className = eleClass;
+		
+		document.getElementById(element.id.replace("-nav", "-content")).style.display = "none";
 	}
 	
-	if (selected.length != 1 || (selected.length == 1 && destElement.className.indexOf("nav-selected") == -1)) {	
+	if (selected.length != 1 || (selected.length == 1 && destNavElement.className.indexOf("nav-selected") == -1)) {	
 		selected.forEach(unselectNavElement);
 	
 	}
@@ -19,5 +22,6 @@ function navigate(destination) {
 		return;
 	}
 	
-	destElement.className = destElement.className + " nav-selected";
+	destNavElement.className = destNavElement.className + " nav-selected";
+	destContentElement.style.display = "block";
 }
