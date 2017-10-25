9175163326794e1ca58a0a3fe3cab993ac0a5edc
Adam Murphy <adam.murphy@mun.ca>
2017-10-25 10:23:27 -0230

    Remove unused navigation javascript

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,32 +1,5 @@
 var BANNER_HEIGHT_PIXELS = 250;
 
-function navigate(destination) {
-	var selectedHTMLCollection = document.getElementsByClassName('nav-selected');
-	var selected = [].slice.call(selectedHTMLCollection);
-
-	var destNavElement = document.getElementById(destination + '-nav');
-	var destContentElement = document.getElementById(destination + '-content');
-
-	function unselectNavElement(element, index, array) {
-		var eleClass = element.className;
-		eleClass = eleClass.replace('nav-selected', '');
-		eleClass = eleClass.replace('  ', ' ').trim();
-		element.className = eleClass;
-
-		document.getElementById(element.id.replace('-nav', '-content')).style.display = 'none';
-	}
-
-	if (selected.length !== 1 || (selected.length === 1 && destNavElement.className.indexOf('nav-selected') === -1)) {
-		selected.forEach(unselectNavElement);
-	}
-	else {
-		return;
-	}
-
-	destNavElement.className = destNavElement.className + ' nav-selected';
-	destContentElement.style.display = 'block';
-}
-
 function onMouseEnterBanner(event) {
 	var banner = event.target;
 	banner.style.transitionDuration = '0.1s, 1s';
